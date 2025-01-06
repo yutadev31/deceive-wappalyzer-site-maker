e6fed2c3302bd0d34bea471a60106d4dac76ddce
Yuta <jp.yuta0131@gmail.com>
2025-01-06 13:00:39 +0900

    add: domをパースする機能を追加

--- gen.py
+++ gen.py
@@ -28,8 +28,36 @@ def replace_chars(s: str):
 
   return s
 
-def proc_dom(s: str):
-  return
+def convert_selector_to_html(selector: str):
+  selector = selector.replace("*", "")
+  # タグ名だけの場合
+  if re.match(r'^[a-zA-Z]+$', selector):  # 単一のタグ名
+    return f'<{selector}></{selector}>'
+
+  # IDが指定された場合 (例: div#id)
+  if re.match(r'^[a-zA-Z0-9]+#[a-zA-Z0-9-]+$', selector):  # div#id形式
+    tag, id_ = selector.split('#')
+    return f'<{tag} id="{id_}"></{tag}>'
+
+  # クラスが指定された場合 (例: .class)
+  if re.match(r'^\.[a-zA-Z0-9-]+$', selector):  # .class形式
+    class_ = selector[1:]  # 先頭の.を削除
+    return f'<div class="{class_}"></div>'
+
+  # クラス指定とタグ名が指定された場合 (例: div.class)
+  if re.match(r'^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9-]+)+$', selector):  # div.class形式
+    tag, class_ = selector.split('.')
+    return f'<{tag} class="{class_}"></{tag}>'
+
+  # 属性が指定された場合 (例: div[attr=value])
+  if re.match(r'^[a-zA-Z0-9]+(?:\[[a-zA-Z0-9-]+=.*\])+$', selector):  # div[attr=value]形式
+    # タグ名と属性部分を分ける
+    tag = selector.split('[')[0]
+    # 属性部分を抽出
+    attrs = re.findall(r'\[([^\]]+)\]', selector)
+    # 属性を処理
+    attributes = ' '.join([f'{attr.split("=")[0]}="{attr.split("=")[1]}"' for attr in attrs])
+    return f'<{tag} {attributes}></{tag}>'
 
 data = None
 html = ""
@@ -76,19 +104,19 @@ for letter in target_files:
       except Exception:
         err += f"cookies: {key}\n"
     if "dom" in item:
-      try:
+      # try:
         dom = item["dom"]
         if isinstance(dom, str):
           doms = dom.split(",")
           for dom_item in doms:
-            html += f"<div>{dom_item.strip()}</div>\n"
+            html += f"{convert_selector_to_html(dom_item.strip())}\n"
         elif isinstance(dom, list):
           for dom_item in dom:
             doms = dom_item.split(",")
             for dom_item in doms:
-              html += f"<div>{dom_item.strip()}</div>\n"
-      except Exception:
-        err += f"dom: {key}\n"
+              html += f"{convert_selector_to_html(dom_item.strip())}\n"
+      # except Exception:
+      #   err += f"dom: {key}\n"
 
 cookies_js = "; ".join(cookies)
 
@@ -96,5 +124,8 @@ html += f"<script>document.cookie = \"{cookies_js}\"</script>"
 
 with open("index.html", "w") as f:
   f.write(html)
+
+with open("err.log", "w") as f:
+  f.write(err)
 
 print("HTMLファイルを生成しました。")
